8e9e205ec67fa4fbad7f5cc4dd8b57dc326c1fc6
teppei <72366088+teppei-kitagawa@users.noreply.github.com>
2024-03-19 10:42:51 +0900

    use Arg

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,25 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("{:?}", std::env::args());
+    let matches = App::new("echor")
+        .version("0.1.0")
+        .author("Teppei Kitagawa <tkitagaw@student.42tokyo.jp>")
+        .about("Rust echo")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text to echo")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("no_newline")
+                .long("no-newline")
+                .short("n")
+                .help("Do not print newline")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    println!("{:#?}", matches)
 }
