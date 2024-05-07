f2a9fe3ff4e99435d10b59e0b3808cb9fe9d03d2
pettei47 <kitagawa.teppei.33c@gmail.com>
2024-05-07 16:25:46 +0900

    open files

--- headr/src/lib.rs
+++ headr/src/lib.rs
@@ -1,5 +1,7 @@
 use clap::{App, Arg};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -41,15 +43,32 @@ pub fn get_args() -> MyResult<Config> {
     )
     .get_matches();
 
+  let lines = matches
+        .value_of("lines")
+        .map(parse_positive_int)
+        .transpose()
+        .map_err(|e| format!("illegal line count -- {}", e))?;
+  
+  let bytes = matches
+        .value_of("bytes")
+        .map(parse_positive_int)
+        .transpose()
+        .map_err(|e| format!("illegal byte count -- {}", e))?;
+
   Ok(Config {
     files: matches.values_of_lossy("files").unwrap(),
-    lines: parse_positive_int(matches.value_of("lines").unwrap())?,
-    bytes: matches.value_of("bytes").map(|s| parse_positive_int(s)).transpose()?,
+    lines: lines.unwrap(),
+    bytes,
   })
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-  println!("{:#?}", config);
+  for filename in config.files {
+    match open(&filename) {
+      Err(e) => eprintln!("{}: {}", filename, e),
+      Ok(_) => println!("Opened {}", filename),
+    }
+  }
   Ok(())
 }
 
@@ -57,6 +76,13 @@ fn parse_positive_int(val: &str) -> MyResult<usize> {
   match val.parse() {
     Ok(n) if n > 0 => Ok(n),
     _ => Err(val.into()),
+  }
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+  match filename {
+    "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+    _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
   }
 }
 
