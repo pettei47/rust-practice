01a7ab6f7a0655a2dc1146f9698b9eb45ed5c3ac
pettei47 <kitagawa.teppei.33c@gmail.com>
2024-06-09 15:35:46 +0900

    fix outputs

--- wcr/src/lib.rs
+++ wcr/src/lib.rs
@@ -1,4 +1,4 @@
-use clap:: {App, Arg};
+use clap::{App, Arg};
 use std::error::Error;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
@@ -7,14 +7,14 @@ type MyResult<T> = Result<T, Box<dyn Error>>;
 
 #[derive(Debug)]
 pub struct Config {
-    files: Vec<String>,
-    lines: bool,
-    words: bool,
-    bytes: bool,
-    chars: bool,
- }
+  files: Vec<String>,
+  lines: bool,
+  words: bool,
+  bytes: bool,
+  chars: bool,
+}
 
- pub fn get_args() -> MyResult<Config> {
+pub fn get_args() -> MyResult<Config> {
   let matches = App::new("wcr")
     .version("0.1.0")
     .author("teppei")
@@ -61,36 +61,46 @@ pub struct Config {
     )
     .get_matches();
 
-    let mut lines = matches.is_present("lines");
-    let mut words = matches.is_present("words");
-    let mut bytes = matches.is_present("bytes");
-    let chars = matches.is_present("chars");
+  let mut lines = matches.is_present("lines");
+  let mut words = matches.is_present("words");
+  let mut bytes = matches.is_present("bytes");
+  let chars = matches.is_present("chars");
 
-    if [lines, words, bytes, chars].iter().all(|&v| !v) {
-      lines = true;
-      words = true;
-      bytes = true;
-    }
+  if [lines, words, bytes, chars].iter().all(|&v| !v) {
+    lines = true;
+    words = true;
+    bytes = true;
+  }
 
-    Ok(Config {
-      files: matches.values_of_lossy("files").unwrap(),
-      lines,
-      words,
-      bytes,
-      chars,
-    })
- }
+  Ok(Config {
+    files: matches.values_of_lossy("files").unwrap(),
+    lines,
+    words,
+    bytes,
+    chars,
+  })
+}
 
 pub fn run(config: Config) -> MyResult<()> {
   for filename in &config.files {
     match open(filename) {
       Err(err) => eprintln!("{}: {}", filename, err),
-      Ok(mut file) => {
+      Ok(file) => {
         let file_info = count(file)?;
-        println!("{:6} {:6} {:6} {}",
-          file_info.num_lines, file_info.num_words,
-          file_info.num_bytes, filename)
-      },
+        if config.lines {
+          print!("{:8}", file_info.num_lines);
+        }
+        if config.words {
+          print!("{:8}", file_info.num_words);
+        }
+        if config.bytes {
+          print!("{:8}", file_info.num_bytes);
+        }
+        if config.chars {
+          print!("{:8}", file_info.num_chars);
+        }
+        println!(" {}", filename)
+      }
     }
   }
   Ok(())
