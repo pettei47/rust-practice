4912249c2c9eec5549fc3cba12cc20e571a9da61
pettei47 <kitagawa.teppei.33c@gmail.com>
2024-07-09 11:09:57 +0900

    pass test stdin

--- uniqr/src/lib.rs
+++ uniqr/src/lib.rs
@@ -1,5 +1,9 @@
 use clap::Parser;
-use std::error::Error;
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -19,6 +23,42 @@ pub struct Config {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:?}", config);
+    let mut file = open(&config.in_file).map_err(|e| format!("{}: {}", config.in_file, e))?;
+    let mut line = String::new();
+    let mut prev_line = String::new();
+    let mut count = 0;
+    loop {
+        let bytes = file.read_line(&mut line)?;
+        // println!("{:?}", bytes);
+        // println!("{:?}", line);
+        // println!("{:?}", prev_line);
+        if prev_line.is_empty() && count == 0 {
+            prev_line = line.clone();
+            line.clear();
+            continue;
+        }
+        count += 1;
+        if line != prev_line {
+            if config.count {
+                println!("{:>4} {}", count, prev_line.replace('\n', ""));
+            } else {
+                println!("{}", prev_line.replace('\n', ""));
+            }
+            count = 0;
+        }
+        if bytes == 0 {
+            break;
+        }
+        prev_line.clear();
+        prev_line = line.clone();
+        line.clear();
+    }
     Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
