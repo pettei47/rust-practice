36cc5f81645418b41b6614cbffa867e5f4c60c0b
pettei47 <kitagawa.teppei.33c@gmail.com>
2024-08-20 10:11:50 +0900

    pass test

--- findr/src/lib.rs
+++ findr/src/lib.rs
@@ -1,7 +1,8 @@
 // use crate::EntryType::*;
-use clap::Parser;
+use clap::{builder::PossibleValue, ArgAction, Parser, ValueEnum};
 use regex::Regex;
 use std::error::Error;
+use walkdir::WalkDir;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -12,38 +13,78 @@ pub enum EntryType {
     Link,
 }
 
+impl ValueEnum for EntryType {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[EntryType::Dir, EntryType::File, EntryType::Link]
+    }
+
+    fn to_possible_value<'a>(&self) -> Option<PossibleValue> {
+        Some(match self {
+            EntryType::Dir => PossibleValue::new("d"),
+            EntryType::File => PossibleValue::new("f"),
+            EntryType::Link => PossibleValue::new("l"),
+        })
+    }
+}
+
 #[derive(Debug, Parser)]
 #[command(version, author, about)]
 /// findr command with Rust
 pub struct Config {
-    /// Search paths (default: ".")
+    /// Search paths
     #[arg(value_name = "PATH", default_value = ".")]
     paths: Vec<String>,
 
-    /// Name
-    #[arg(short, long = "name", value_name = "NAME")]
+    /// Names
+    #[arg(
+        short('n'),
+        long("name"),
+        value_name = "NAME",
+        value_parser(Regex::new),
+        action(ArgAction::Append),
+        num_args(0..)
+    )]
     names: Vec<Regex>,
 
-    /// Entry type [possible values: f, d, l]
+    /// Entry types
     #[arg(
-      short = 't',
-      long = "type",
-      value_name = "TYPE",
-      value_parser = parse_entry_type,
+        short('t'),
+        long("type"),
+        value_name = "TYPE",
+        value_parser(clap::value_parser!(EntryType)),
+        action(ArgAction::Append),
+        num_args(0..)
     )]
     entry_types: Vec<EntryType>,
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:?}", config);
-    Ok(())
-}
-
-fn parse_entry_type(s: &str) -> Result<EntryType, String> {
-    match s {
-        "f" => Ok(EntryType::File),
-        "d" => Ok(EntryType::Dir),
-        "l" => Ok(EntryType::Link),
-        _ => Err("Invalid entry type".to_string()),
+    for path in config.paths {
+        for entry in WalkDir::new(path) {
+            match entry {
+                Err(e) => eprintln!("{}", e),
+                Ok(entry) => {
+                    let entry_type = if entry.file_type().is_dir() {
+                        EntryType::Dir
+                    } else if entry.file_type().is_file() {
+                        EntryType::File
+                    } else if entry.file_type().is_symlink() {
+                        EntryType::Link
+                    } else {
+                        continue;
+                    };
+                    if (config.entry_types.is_empty() || config.entry_types.contains(&entry_type))
+                        && (config.names.is_empty()
+                            || config
+                                .names
+                                .iter()
+                                .any(|re| re.is_match(entry.file_name().to_str().unwrap())))
+                    {
+                        println!("{}", entry.path().display());
+                    }
+                }
+            }
+        }
     }
+    Ok(())
 }
