b5932831f96e275eb75a4f0a2fcc4034d1511bfa
pettei47 <kitagawa.teppei.33c@gmail.com>
2025-04-23 13:42:09 +0900

    add tests

--- calr/src/main.rs
+++ calr/src/main.rs
@@ -1,3 +1,11 @@
+use clap::Parser;
+use calr::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::parse();
+
+    if let Err(e) = calr::run(config) {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
 }
